28b6a4413e3c5bb3e165b733f04dcef35a5f5b38
Eriko Anggara <erikoanggara@gmail.com>
2019-12-02 16:58:23 +0700

    inventory basic bag
    Only open and close inventory

---
+++ Assets/Scripts/Inventory/OpenBag.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenBag: MonoBehaviour
+{
+    public Button bagButton;
+    public Image bagContent;
+    public Image item1;
+    public Image item2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bagContent.enabled = false;
+        item1.enabled = false;
+        item2.enabled = false;
+        bagButton.onClick.AddListener(ButtonClicked);
+    }
+
+    void ButtonClicked()
+    {
+        bagContent.enabled = !bagContent.enabled;
+        item1.enabled = !item1.enabled;
+        item2.enabled = !item2.enabled;
+    }
+}
