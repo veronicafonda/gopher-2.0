3a084a28da6732157c376f7346e74419057578fd
TimothyFajarki <timothyfajarki@gmail.com>
2019-12-06 09:28:21 +0700

    Dialog Box Follows Char
    Dialog Box mengikuti Stuart

---
+++ Assets/Scripts/UI_Placeholder.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Placeholder : MonoBehaviour
+{
+
+    public Image DialogBox;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 namePos = Camera.main.WorldToScreenPoint(this.transform.position);
+        DialogBox.transform.position = namePos;
+    }
+}
