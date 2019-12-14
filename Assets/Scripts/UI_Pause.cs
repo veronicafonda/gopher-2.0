2d1e5becd8869ce7d7a70c9d71ff69f7d86fe74f
TimothyFajarki <timothyfajarki@gmail.com>
2019-12-14 21:15:11 +0700

    Menu Pause (not ready)
    Pembuatan menu pause

---
+++ Assets/Scripts/UI_Pause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Pause : MonoBehaviour
+{
+    public Animator pauseUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onClickPause()
+    {
+        if(pauseUI.GetBool("PauseOpen") == false)
+        {
+            pauseUI.SetBool("PauseOpen", true);
+        }
+        else
+        {
+            pauseUI.SetBool("PauseOpen", false);
+            Time.timeScale = 1;
+        }
+        
+    }
+}
