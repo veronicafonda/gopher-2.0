cb136ba9a6c59f08934503273d25b255b125e9b1
Eriko Anggara <erikoanggara@gmail.com>
2019-12-15 10:23:22 +0700

    Udah bisa buka nutup buttonnya

--- Assets/Scripts/OpenCloseBag.cs
+++ Assets/Scripts/OpenCloseBag.cs
@@ -7,15 +7,22 @@ public class OpenCloseBag : MonoBehaviour
 {
     public Button bagButton;
     public Image bagContent;
+    public Image Item1;
+    public Image Item2;
+
     // Start is called before the first frame update
     void Start()
     {
         bagContent.enabled = false;
+        Item1.enabled = false;
+        Item2.enabled = false;
         bagButton.onClick.AddListener(InventoryController);
     }
     
     void InventoryController()
     {
         bagContent.enabled = !bagContent.enabled;
+        Item1.enabled = !Item1.enabled;
+        Item2.enabled = !Item2.enabled;
     }
 }
