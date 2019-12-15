4dff4e74f94502bd0255daf0d5b6ec8540edc569
Xerather <alvin.janitra@gmail.com>
2019-12-15 16:31:20 +0700

    update dialog

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public Dialogue dialogue;
+    //public Dialogue dialogue;
+    public List<Dialogue> dialogue;
+    public int status_dialogue;
+    public int counter;
+    public Dialogue default_dialogue;
+
+
+    public void Start()
+    {
+        status_dialogue = 0;
+        counter = dialogue.Count;
+
+        Debug.Log(counter);
+
+    }
 
     public void TriggerDialogue ()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        //FindObjectOfType<DialogueManager>().StartDialogue(dialogue[1]);
+
+        if ( status_dialogue < counter)
+        {   
+            if(dialogue[status_dialogue].when == sc_hero.levelProgress)
+            {
+                //Debug.Log("keluar +" + status_dialogue );
+                FindObjectOfType<DialogueManager>().StartDialogue(dialogue[status_dialogue]);
+                
+            }
+            status_dialogue++;
+        }
+        else
+        {
+            FindObjectOfType<DialogueManager>().StartDialogue(default_dialogue);
+        }
     }
 }
